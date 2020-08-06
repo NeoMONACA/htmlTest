a9d4c57a95020400286c881f113d897003e95f40
HA <o3ne.ozon.aoki@gmail.com>
2020-08-07 02:20:12 +0900

    Studied HTML and CSS Hello World and Hello VEGAS

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <meta charaset="utf-8"/>
+    <title>kapi-chan</title>
+
+    <style>
+    h1{
+        color:red;
+    }
+    </style>
+
+</head>
+    <body>
+        <h1>Hello world!!!</h1>
+        <p>はじめてのwebサイトへようこそ</p>
+            <h2>Hello VEGAS</h2>
+                <img src="https://pix8.agoda.net/hotelImages/50345/-1/673b2a5f817d65090d3b33d635e61ca8.jpg?s=713x768"
+                alt="wynn las vegas">
+                    <ul>ベガスに行くメリットとは？
+                        <li>ポーカーのレーキが安い</li>
+                        <li>カジノ以外も楽しめる</li>
+                        <li>世界最高峰のエンターテイメントが集結</li>
+                    </ul>
+
+    </body>
+</html>
